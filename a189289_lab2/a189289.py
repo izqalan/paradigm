c1b7e04aab8d17112d387702979907b5455165c1
Izqalan Nor'Izad <izqalan@gmail.com>
2024-04-03 10:19:36 +0800

    lab2

--- a189289_lab2/a189289.py
+++ a189289_lab2/a189289.py
@@ -2,12 +2,15 @@
 # Author: Izqalan Nor'Izad
 # Matric: a189289
 
-personal_tuple = [('name', 'izqalan1'),('age', 25), ('drinks', 'negroni')]
+personal_tuple = [('name', 'izqalan'),('age', 25), ('drinks', 'negroni')]
 personal_dict = dict(personal_tuple)
 
-print(f'Name: {personal_dict.get("name")}')
-print(f'Age: {personal_dict.get("age")}')
-print(f'Favorite drink: {personal_dict.get("drinks")}')
+def get_key(value):
+  for k,v in personal_dict.items():
+    if v == value:
+      return k
+  return 'key not found'
 
-
-
+print(get_key('izqalan'))
+print(get_key(25))
+print(get_key('negroni'))
