72a771d8e48fec69cb033541c36b082cc7bd441c
Izqalan Nor'Izad <izqalan@gmail.com>
2024-04-24 12:08:13 +0800

    lab4: removed boilerplate codes

--- a189289_a189310_a186277_lab4/a189289_a189310_a186277_LT4.py
+++ a189289_a189310_a186277_lab4/a189289_a189310_a186277_LT4.py
@@ -8,25 +8,12 @@ NURUDDIN NAIM BIN ABU HANIFAH(A186277)
 '''
 
 import turtle               # allows us to use the turtles library                                         
-wn = turtle.Screen()        # creates a graphics window (canvas)                                                   
-                                           
-def flower(jane):           # This function is a sample, you can delete it
-    for i in range(12):     
-        # draw a square
-        jane.forward(50)
-        jane.left(90)
-        jane.forward(50)
-        jane.left(90)
-        jane.forward(50)
-        jane.left(90)
-        jane.forward(50)
-        jane.left(90) 
-        jane.left(30)
-                                                                                              
+wn = turtle.Screen()                   
+                                                                                     
 def mainDraw():
-    jane = turtle.Turtle()    # you can rename the turtle if you want
-    flower(jane)   
- 
+  # fill color
+  wn.bgcolor("blue")
+
 if __name__ == '__main__':
     mainDraw() 
 
