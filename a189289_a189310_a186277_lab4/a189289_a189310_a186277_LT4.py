dce47b9cdbe835c050157c121b179cc998c388f7
Wilson-Lam123 <96438060+Rin-210@users.noreply.github.com>
2024-04-24 12:39:06 +0800

    Update a189289_a189310_a186277_LT4.py

--- a189289_a189310_a186277_lab4/a189289_a189310_a186277_LT4.py
+++ a189289_a189310_a186277_lab4/a189289_a189310_a186277_LT4.py
@@ -8,13 +8,35 @@ NURUDDIN NAIM BIN ABU HANIFAH(A186277)
 '''
 
 import turtle               # allows us to use the turtles library                                         
-wn = turtle.Screen()                   
-                                                                                     
+canvaWindow = turtle.Screen()            # creates a graphics window (canvas)        
+                                                                                      
+                                                      
+
+def drawRectangle(t,x,y,w,h,colorP="black",colorF="white"):
+    t.pencolor(colorP)
+    t.fillcolor(colorF)
+    t.up()
+    t.goto(x,y)
+    t.down()
+    t.begin_fill()
+    t.goto(x+w,y)
+    t.goto(x+w,y+h)
+    t.goto(x,y+h)
+    t.goto(x,y)
+    t.end_fill()
+                                                                                       
 def mainDraw():
-  # fill color
-  wn.bgcolor("blue")
-
+    canvaWindow.colormode(255) 
+    canvaWindow.bgcolor(17,178,255)      # fill color
+    
+    draw_pen = turtle.Turtle()    # you can rename the turtle if you want
+    
+    drawRectangle(draw_pen,-200,-300,500,500,"black","blue")
+    drawRectangle(draw_pen,0,0,300,300,"yellow","red")
+ 
 if __name__ == '__main__':
-    mainDraw() 
+    mainDraw()
 
-wn.exitonclick()
+
+
+canvaWindow.exitonclick()
