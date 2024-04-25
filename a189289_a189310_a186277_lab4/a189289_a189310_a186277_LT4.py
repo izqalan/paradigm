8719955a7e39c3fda57b760ff6328689c0bfbae2
izqalan <izqalan@gmail.com>
2024-04-25 21:43:55 +0800

    lab4: add triangle function

--- a189289_a189310_a186277_lab4/a189289_a189310_a186277_LT4.py
+++ a189289_a189310_a186277_lab4/a189289_a189310_a186277_LT4.py
@@ -7,9 +7,72 @@ Izqalan Nor'Izad (A189289)
 Nuruddin Naim Bin Abu Hanifah(A186277)
 '''
 
-import turtle               # allows us to use the turtles library                                         
-canvaWindow = turtle.Screen()            # creates a graphics window (canvas)        
-                                                                                      
+import turtle 
+
+# this will create an empty window canvas for our digital aquarium lol
+canvaWindow = turtle.Screen()
+
+# Hello team, please add documentation like this
+# So that we can understand what the function does
+# and how to use it
+# - @izqalan
+
+# Triangle function
+# Parameters: t - turtle object
+#             x,y - starting position of the triangle
+#             size - size of the triangle
+#             colorP - pen color
+#             colorF - fill color
+#             point - left, right, or up. default is down
+# Return: None
+def drawTriangle(t,x,y,size,colorP="black",colorF="white", point="up"):
+    t.pencolor(colorP) 
+    t.fillcolor(colorF)
+    # Lift the pen up - no drawing when moving
+    t.up()
+    t.begin_fill() # Start filling in the shape
+    
+    point = point.lower()
+
+    if point == "down":
+        # go to point edge, y-size because we want to draw from the bottom
+        t.goto(x, y-size)
+        t.down()
+        # draw to right up edge, hence the x+size (right) and y+size (up)
+        t.goto(x+size, y+size)
+        # draw to left (x-size) edge and maintain current y position as before (y+size)
+        t.goto(x-size, y+size)
+        # draw back to the starting point
+        t.goto(x, y-size)
+    elif point == "left":
+        # go left from the starting point x
+        t.goto(x-size, y)
+        t.down()
+        # draw to the right x+ and up edge y+
+        t.goto(x+size, y+size)
+        # go down (y-) and maintain x plane
+        t.goto(x+size, y-size)
+        # go back to starting point. left x- and up y plane
+        t.goto(x-size, y)
+    elif point == "right":
+        # uhhh, basically just an inverse of left
+        t.goto(x+size, y)
+        t.down()
+        t.goto(x-size, y+size)
+        t.goto(x-size, y-size)
+        t.goto(x+size, y)
+    else:
+        # default is up
+        # this is basically the same as down but in reverse
+        t.goto(x, y+size)
+        t.down()
+        t.goto(x+size, y-size)
+        t.goto(x-size, y-size)
+        t.goto(x, y+size)
+    
+    t.up()
+    t.end_fill() # End filling in the shape
+
 def drawRectangle(t,x,y,w,h,colorP="black",colorF="white"):
     t.pencolor(colorP)
     t.fillcolor(colorF)
@@ -29,9 +92,16 @@ def mainDraw():
     
     draw_pen = turtle.Turtle()    # you can rename the turtle if you want
     
-    drawRectangle(draw_pen,-200,-300,500,500,"black","blue")
-    drawRectangle(draw_pen,0,0,300,300,"yellow","red")
- 
+    # drawRectangle(draw_pen,-200,-300,500,500,"black","blue")
+    # drawRectangle(draw_pen,0,0,300,300,"yellow","red")
+    # drawTriangle(draw_pen,0,0,100,"black","blue",0)
+    # drawTriangle(draw_pen,0,100,75,"black","red", "down")
+    # drawTriangle(draw_pen,0,0,10,"black","blue")
+    drawTriangle(draw_pen,0,-100,50,"black","red", "left")
+    # drawTriangle(draw_pen,0,-200,50,"black","red", "right")
+    # drawTriangle(draw_pen,0,100,100,"black","red", "left")
+
+
 if __name__ == '__main__':
     mainDraw()
 
