2aa793dc974f8932de6026ae5ea16574f9cdba4b
izqalan <izqalan@gmail.com>
2024-06-02 16:13:19 +0800

    lab6: free memory alloc for sales

--- lab6/LAB02-TASK01.c
+++ lab6/LAB02-TASK01.c
@@ -13,13 +13,18 @@ Nuruddin Naim Bin Abu Hanifah (A186277)
 struct Employee {
     int id;
     char name[50];
-    float sales[DAYS_IN_A_WEEK];
+    float *sales;
     float totalSales;
 };
 
 // Requirements states inputSales only take one parameter which is a pointer to an Employee structure
 void inputSales(struct Employee *employees) {
-        
+    
+    employees->sales = (float *)malloc(DAYS_IN_A_WEEK * sizeof(float));
+    if (employees->sales == NULL) {
+        printf("Error: Memory allocation failed\n");
+        exit(1);
+    }
     // Input validation for ID,check it must be integer
     printf("ID: ");
     while (scanf("%d", &employees->id) != 1) {
@@ -67,9 +72,9 @@ float calculateOverallTotalSales(struct Employee *employees, int numEmployees) {
     return overallTotalSales;
 }
 
-struct Employee* findEmployeeWithHighestSales(struct Employee *employees) {//find top sale employee
+struct Employee* findEmployeeWithHighestSales(struct Employee *employees, int numEmployees) {//find top sale employee
     struct Employee *topSaleEmployee = &employees[0];
-    for (int i = 1; i < DAYS_IN_A_WEEK; i++) {
+    for (int i = 1; i < numEmployees; i++) {
         if (employees[i].totalSales > topSaleEmployee->totalSales) {
             topSaleEmployee = &employees[i];
         }
@@ -109,14 +114,18 @@ int main() {
     printf("\n\nOverall Total Sales for the Week: RM%.2f\n", overallTotalSales);
 
     // top sale employee
-    struct Employee *topSaleEmployee = findEmployeeWithHighestSales(employees);
+    struct Employee *topSaleEmployee = findEmployeeWithHighestSales(employees, numEmployees);
     printf("\nEmployee with the Highest Sales:\n");
     printf("ID: %d\n", topSaleEmployee->id);
     printf("Name: %s\n", topSaleEmployee->name);
     printf("Total Sales: RM%.2f\n", topSaleEmployee->totalSales);
 
+    for (int i = 0; i < numEmployees; i++) {
+        free(employees[i].sales);
+    }
     // free memory
     free(employees);
+
 
     printf("\n\nThank you for using our Sales Tracking System!\n");
     return 0;
